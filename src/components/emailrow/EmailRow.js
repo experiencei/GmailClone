e9783987433622f6f5e546a00f25e100101bad8b
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 20:31:14 +0100

    pulling data from database

--- src/components/emailrow/EmailRow.js
+++ src/components/emailrow/EmailRow.js
@@ -1,15 +1,24 @@
 import { Checkbox, IconButton } from '@material-ui/core';
 import { LabelImportantOutlined, StarBorderOutlined } from '@material-ui/icons';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
+import { selectMail } from '../../features/mailSlice';
 import "./EmailRow.css";
 
-function EmailRow( { title , subject , description , time}) {
-
+function EmailRow( { id , title , subject , description , time}) {
+  const dispatch = useDispatch();
   const  history = useHistory();
 
+  const openMail = () => {
+      dispatch(selectMail({
+        id , title , subject , description , time
+      }));
+
+      history.push("/mail")
+  }
     return (
-        <div onClick={ () => history.push("/mail")} className="emailrow">
+        <div onClick={openMail} className="emailrow">
             <div className="emailRow__options">
                <Checkbox/>
                <IconButton>

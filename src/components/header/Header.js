244004510fd6afd87f011fcae8ac6a0c5d4d9b26
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 14:58:55 +0100

    using material ui to style the header components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,25 @@
+import { IconButton, Menu } from '@material-ui/core';
 import React from 'react'
 import "./Header.css";
-import
+
 function Header() {
     return (
         <div className="header">
-            <h2><Menu</h2>
+              <div className="header__left">
+                 <IconButton>
+                     <Menu/>
+                 </IconButton>
+                 <img
+                     src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+                     alt=""
+                 />
+              </div>
+              <div className="header__middle">
+
+              </div>
+              <div className="header__right">
+
+              </div>
         </div>
     )
 }

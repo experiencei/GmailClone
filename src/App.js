939a6c447756ce805e648e3544ae1854027eada7
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 15:59:47 +0100

    using react router dom for page navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { selectUser , login ,logout } from './features/userSlice';
+import { BrowserRouter as Router , Switch , Link , Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
+    < Router>
     <div className="app">
     <Header/>
+    <div className="app__body">
     <Sidebar/>
-    <
+
+    <Switch>
+        <Route path="/mail">
+           <Mail/>
+        </Route>
+        <Route path="/">
+            <EmailList/>
+        </Route>
+    </Switch>
     </div>
+    </div>
+    </Router>
   );
 }
 

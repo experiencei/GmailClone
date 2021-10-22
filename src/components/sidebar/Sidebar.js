9db89af719aa0a0be50d2d3915545e90e0df8887
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 15:50:46 +0100

    using material ui to style the header components

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button } from '@material-ui/core';
-import { AccessTime, Add, ExpandMore, Inbox, LabelImportant, NearMe, Note, Star } from '@material-ui/icons';
+import { Button , IconButton } from '@material-ui/core';
+import { AccessTime, Add, Duo, ExpandMore, Inbox, LabelImportant, NearMe, Note, Person, Phone, Star } from '@material-ui/icons';
 import "./Sidebar.css";
 import SidebarOption from '../sidebarOption/SidebarOption';
 
@@ -17,6 +17,20 @@ function Sidebar() {
             <SidebarOption Icon={NearMe} title="Sent" number={24}/>
             <SidebarOption Icon={Note} title="Drafts" number={154}/>
             <SidebarOption Icon={ExpandMore} title="More" number={4}/>
+
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                     <IconButton>
+                         <Person/>
+                     </IconButton>
+                     <IconButton>
+                         <Duo/>
+                     </IconButton>
+                     <IconButton>
+                         <Phone/>
+                     </IconButton>
+                </div>
+            </div>
         </div>
     )
 }

0b6544f4a7c70a54fdcdfc8547ed24ed26a18606
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 14:48:55 +0100

    using material ui to style the header components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Header.css";
-
+import
 function Header() {
     return (
         <div className="header">
-            <h2>Thsi</h2>
+            <h2><Menu</h2>
         </div>
     )
 }

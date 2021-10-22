f291a02d9aac5f735b66929289c27a69fc74b7f0
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 15:33:12 +0100

    working on the sidebar component

--- src/components/sidebarOption/SidebarOption.js
+++ src/components/sidebarOption/SidebarOption.js
@@ -5,6 +5,8 @@ function SidebarOption( { Icon , title , number}) {
     return (
         <div className="sidebarOption">
             <Icon/>
+            <h3>{title}</h3>
+            <p>{number}</p>
         </div>
     )
 }

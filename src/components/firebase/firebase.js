d681c22a30f8cb58ee40274a479b005e59d2380c
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 19:47:54 +0100

    using firebase and input attribute to populate the input page

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAwiFzeFi4fUEUTZRMNJfhIJqltHDUwiDk",
     authDomain: "clone-7d120.firebaseapp.com",
@@ -6,3 +10,8 @@ const firebaseConfig = {
     messagingSenderId: "477155898217",
     appId: "1:477155898217:web:7e8b9b005c0e08068543f8"
   };
+
+firebase.initializeApp(firebaseConfig);
+ 
+export const auth = firebase.auth();
+export const db = firebase.firestore();

fb4ad7f7c674ee65b1b1d5d0acfc26edfacc94fe
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 14:47:46 +0100

    using material ui to style the header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { selectUser , login ,logout } from './features/userSlice';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="app">
+    <Header/>
       <h2>Hello yihua</h2>
     </div>
   );

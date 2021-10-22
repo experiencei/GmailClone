fc41266ab845e52de37ee719663d2bcd3948bd42
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 16:22:21 +0100

    using material ui to style the header components

--- src/components/email/EmailList.js
+++ src/components/email/EmailList.js
@@ -1,5 +1,5 @@
 import { Checkbox , IconButton } from '@material-ui/core';
-import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, MoreVert, Redo, Settings } from '@material-ui/icons';
+import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@material-ui/icons';
 import React from 'react'
 import Section from '../section/Section';
 import "./EmailList.css";
@@ -38,6 +38,8 @@ function EmailList() {
 
             <div className="emailist__sections">
                 <Section Icon={Inbox} title="Primary" color="red" selected/>
+                <Section Icon={People} title="Social" color="#1A73E8" />
+                <Section Icon={LocalOffer} title="Promotions" color="green"/>
             </div>
         </div>
     )

c46fa09e5872cb20271e3608c5e9bdef9ada7f34
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 17:50:22 +0100

    using firebase and input attribute to populate the input page

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwiFzeFi4fUEUTZRMNJfhIJqltHDUwiDk",
+    authDomain: "clone-7d120.firebaseapp.com",
+    projectId: "clone-7d120",
+    storageBucket: "clone-7d120.appspot.com",
+    messagingSenderId: "477155898217",
+    appId: "1:477155898217:web:7e8b9b005c0e08068543f8"
+  };

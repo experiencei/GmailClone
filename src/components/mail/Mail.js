d77f538ae71e29fce6280f787ba86880f7bb28ab
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 17:20:31 +0100

    using material ui to style the header components

--- src/components/mail/Mail.js
+++ src/components/mail/Mail.js
@@ -1,10 +1,57 @@
+import { IconButton } from '@material-ui/core';
+import { ArrowBack, CheckCircle, Delete, Email, Error, MoreVert, MoveToInbox, WatchLater } from '@material-ui/icons';
 import React from 'react'
+import { useHistory } from 'react-router';
 import "./Mail.css";
 
 function Mail() {
+   const history = useHistory();
+
     return (
         <div className="mail">
-            <h1>What going </h1>
+           <div className="mail__tools">
+               <div className="mail__toolsLeft">
+                    <IconButton onClick={ () => history.push("/")}>
+                        <ArrowBack/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoveToInbox/>
+                    </IconButton>
+
+                    <IconButton>
+                        <Error/>
+                    </IconButton>
+
+                    <IconButton>
+                        <Delete/>
+                    </IconButton>
+
+                    <IconButton>
+                        <Email/>
+                    </IconButton>
+
+                    <IconButton>
+                        <WatchLater/>
+                    </IconButton>
+
+                    <IconButton>
+                        <CheckCircle/>
+                    </IconButton>
+
+                    <IconButton>
+                        <LabelImportant/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+               </div>
+               <div className="mail__toolsRight">
+
+               </div>
+           </div>
+           <h1>Hello </h1>
         </div>
     )
 }

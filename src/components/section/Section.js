3298fb465abf1ad0fcab272ce622ae4e3c5c4cec
experiencei <ayelojahighbee01@gmail.com>
2021-10-22 16:27:40 +0100

    using material ui to style the header components

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -8,7 +8,8 @@ function Section({ Icon , title , color , selected }) {
           style={{ borderBottom : `3px solid ${color}`,
                   color: `${selected && color}`
           }}>
-            
+            <Icon/>
+            <h4>{title}</h4>
         </div>
     )
 }
